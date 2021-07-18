5704606ca1bb23695c035638dc775b5ce51e70ff
arikarim <arikarim146@gmail.com>
2021-07-18 12:17:01 +0300

    Update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -11,6 +11,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
     resource '*',
              headers: :any,
-             methods: %i[get post put patch delete options head]
+             methods: %i[get post put patch delete options head],
+             credentials: true
   end
 end
