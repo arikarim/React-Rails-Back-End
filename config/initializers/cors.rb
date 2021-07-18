46192ea59eb2d31f1ec71775e353af39467a70c7
arikarim <arikarim146@gmail.com>
2021-07-18 22:41:33 +0300

    Update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -16,12 +16,12 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
              credentials: true
   end
 
-  # allow do
-  #   origins 'https://fierce-tundra-36709.herokuapp.com'
+  allow do
+    origins 'https://fierce-tundra-36709.herokuapp.com'
 
-  #   resource '*',
-  #            headers: :any,
-  #            methods: %i[get post put patch delete options head],
-  #            credentials: true
-  # end
+    resource '*',
+             headers: :any,
+             methods: %i[get post put patch delete options head],
+             credentials: true
+  end
 end
