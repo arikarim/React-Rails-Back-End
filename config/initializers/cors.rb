f75d45018f225259eda760ad2494c935521160d5
arikarim <arikarim146@gmail.com>
2021-07-18 12:28:28 +0300

    Update ports

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,16 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'http://localhost:3000'
+
+    resource '*',
+             headers: :any,
+             methods: %i[get post put patch delete options head],
+             credentials: true
+  end
+
+  allow do
+    origins 'https://fierce-tundra-36709.herokuapp.com'
 
     resource '*',
              headers: :any,
