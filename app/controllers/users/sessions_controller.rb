7d75b8e265783070f24cceee868d9e96dacd9bb2
arikarim <arikarim146@gmail.com>
2021-07-18 10:58:05 +0300

    Update messeges

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,7 +4,11 @@ class Users::SessionsController < Devise::SessionsController
   private
 
   def respond_with(_resource, _opts = {})
-    render json: { message: 'You are logged in.', user: current_user }, status: :ok
+    if current_user
+      render json: { message: 'You are logged in.', user: current_user }, status: :ok
+    else
+      render json: { message: 'Theres no user, You are not logged in.' }, status: :unauthorized
+    end
   end
 
   def respond_to_on_destroy
