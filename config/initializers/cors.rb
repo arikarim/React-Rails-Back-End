c6e35e7ad558ade0e6f2186306b42e67e0669197
arikarim <arikarim146@gmail.com>
2021-07-18 13:51:55 +0300

    Test

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -11,6 +11,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
     resource '*',
              headers: :any,
+             expose: ["Authorization"],
              methods: %i[get post put patch delete options head],
              credentials: true
   end
