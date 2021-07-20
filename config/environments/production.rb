4a7b7de4d33dcf0bec2b8d44120aa79770bf103e
arikarim <arikarim146@gmail.com>
2021-07-20 14:26:57 +0300

    ok

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 require "active_support/core_ext/integer/time"
-
+Rails.application.config.hosts = nil
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
