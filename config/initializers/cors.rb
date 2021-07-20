529086e92dcd9a463eef401738f00d71ffd377cf
arikarim <arikarim146@gmail.com>
2021-07-20 13:53:14 +0300

    ok

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://afternoon-coast-71095.herokuapp.com/'
+    origins 'http://localhost:3000/'
 
     resource '*', headers: :any,
                   methods: :any,
