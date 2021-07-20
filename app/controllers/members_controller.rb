123baff885f8b6d3388b7465e8d517134a72a139
arikarim <arikarim146@gmail.com>
2021-07-20 15:33:32 +0300

    aa

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,5 +1,5 @@
 class MembersController < ApplicationController
-  before_filter :authenticate_user!
+  before_action :authenticate_user!
 
   def show
     render json: {user: current_user, message: "Yeppa You did it" }
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,11 +7,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins '*'
 
     resource '*', headers: :any,
                   methods: :any,
-                  expose: :any,
-                  credentials: true
+                  expose: :any
+                  
   end
 end
