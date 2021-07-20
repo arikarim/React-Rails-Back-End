6037bdd8ff4b9c95b40b35a01d156d7cfeaa4bf6
arikarim <arikarim146@gmail.com>
2021-07-20 12:47:40 +0300

    Update

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable,
-         :jwt_authenticatable,
-         :registerable,
-         jwt_revocation_strategy: JwtDenylist
+  :jwt_authenticatable, jwt_revocation_strategy: Denylist
 
   def generate_jwt
     JWT.encode({id: id, exp: 60.days.from_now.to_i}, Rails.application.secrets.secret_key_base)
