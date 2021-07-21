6f8ad441e173a44a3aaa7216876ce4886a15bbc6
arikarim <arikarim146@gmail.com>
2021-07-21 13:20:35 +0300

    ssss

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:30000'
+    origins 'http://localhost:3000'
 
     resource '*', headers: :any,
                   methods: :any,
