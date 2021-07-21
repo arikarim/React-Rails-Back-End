520b06203ceb0e3f5ebf745422ff55895aa543d1
arikarim <arikarim146@gmail.com>
2021-07-21 22:45:25 +0300

    sssssss

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
+Rails.application.config.middleware.insert_before 0, Rack::Cors, debug: true do
   allow do
     origins 'http://localhost:3000'
 
