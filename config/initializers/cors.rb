c44a18550a2ecaded5347c35dab0c9fbb9084987
arikarim <arikarim146@gmail.com>
2021-07-22 10:57:51 +0300

    sss

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,61 +6,14 @@
 # Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
 
 # Read more: https://github.com/cyu/rack-cors
-domain = 'example.com'
 
-# These subdomains are not trusted and should not be allowed to authenticate with cookies
-cors_third_party_subdomains = %w[
-  blog
-  shop
-  support
-].join('|')
-
-# These are the Rack::Cors settings that we want to set, first for all domains and then for trusted ones
-cors_headers = {
-  resource: '*', headers: :any,
-  methods: :any,
-  expose: %w[Authorization],
-  credentials: false
-}
-cors_headers_internal = cors_headers.merge({ credentials: true })
-
-# This is the actual Rack::Cors configuration
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  # Third party subdomains should not get cookies in case of XSS
+Rails.application.config.middleware.insert_before 0, Rack::Cors, debug: true do
   allow do
-    origins %r{^https://(#{cors_third_party_subdomains})\.#{domain}$}
-    resource '*', cors_headers
-  end
+    origins 'http://localhost:3000'
 
-  # We only want allow-credentials to be true for our own requests
-  # Otherwise you'll need to supply or other non-cookie credentials
-  allow do
-    origins %r{^https://(|[^.]+\.)#{domain}$}
-    resource '*', cors_headers_internal
-  end
-
-  # All other requests made over https allow CORS without credentials
-  allow do
-    origins %r{^https://}
-    resource '*', cors_headers
-  end
-
-  # Allow connections from localhost on non-prod environments as internal requests
-  unless Rails.env.production?
-    allow do
-      origins %r{^(https?://)?localhost(:\d+)?$}
-      resource '*', cors_headers_internal
-    end
+    resource '*', headers: :any,
+                  methods: :any,
+                  expose: %w[Authorization],
+                  credentials: false
   end
 end
-
-# Rails.application.config.middleware.insert_before 0, Rack::Cors, debug: true do
-#   allow do
-#     origins 'http://localhost:3000'
-
-#     resource '*', headers: :any,
-#                   methods: :any,
-#                   expose: %w[Authorization],
-#                   credentials: true
-#   end
-# end
