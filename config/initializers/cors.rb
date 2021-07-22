c21af577e4a35f7d491cb4f869819c2300b60bbc
arikarim <arikarim146@gmail.com>
2021-07-22 11:43:26 +0300

    zz

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -11,7 +11,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors, debug: true do
   allow do
     origins '*'
 
-    resource '*', headers: :any,
+    resource '*', headers: %w(Authorization),
                   methods: :any,
                   expose: %w[Authorization]
   end
