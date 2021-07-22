d4f8c2943f25c741e090a9e1c9e252e6fefb3d7b
arikarim <arikarim146@gmail.com>
2021-07-22 13:47:19 +0300

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :posts
   devise_for :users, controllers: {
     registrations: :registrations,
     sessions: :sessions
